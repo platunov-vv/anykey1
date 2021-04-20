63b16e37c57742f87207fa5f0af48049c621ee0d
platunov-vv <platunov.vv@gmail.com>
2021-04-20 21:37:06 +0300

    +swagger http://localhost:8081/swagger-ui.html - служебные эндпоинты

--- src/main/java/com/anykey/config/SwaggerConfig.java
+++ src/main/java/com/anykey/config/SwaggerConfig.java
@@ -14,7 +14,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.anykey.web"))
                 .paths(PathSelectors.any())
                 .build();
     }
