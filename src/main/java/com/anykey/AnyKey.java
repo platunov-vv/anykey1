920592f48a40a898376cdf664556ed0cea8d1796
platunov-vv <platunov.vv@gmail.com>
2021-04-20 20:53:04 +0300

    +swagger http://localhost:8081/swagger-ui.html

--- src/main/java/com/anykey/AnyKey.java
+++ src/main/java/com/anykey/AnyKey.java
@@ -1,7 +1,10 @@
 package com.anykey;
 
+import com.anykey.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -9,5 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class AnyKey {
     public static void main(String[] args) {
         SpringApplication.run(AnyKey.class, args);
+        ApplicationContext ctx =
+                new AnnotationConfigApplicationContext(SwaggerConfig.class);
+
     }
 }
